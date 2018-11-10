0b903173f237cc331273741300f001f10e0b0dab
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-10 21:03:33 +0530

    Rental Card, Separate Concerns

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import  Header  from './shared/Header';
+import RentalCard from './components/rental/RentalCard';
 
 class App extends Component {
   render() {
     return(
       <div>
         <Header />
-        This is main container
+        <div className="container">
+          <section id="rentalListing">
+            <h1 className="page-title"> Your Home All Around the World</h1>
+            <div className="row">
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+            </div>
+          </section>
+        </div>
       </div>
     )
   }
---
+++ src/components/rental/RentalCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const RentalCard = () =>  {
+    return (
+        <div className="col-md-3 col-xs-6">
+        <div className="card bwm-card">
+            <img className="card-img-top" src="http://via.placeholder.com/350x250" alt=""/>
+            <div className="card-block">
+            <h6 className="card-subtitle">Whole Apartment &#183; New York</h6>
+            <h4 className="card-title">Some nice apartment</h4>
+            <p className="card-text">$240 per Night &#183; Free Cancelation</p>
+            <a href="#" className="card-link">More Info</a>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default RentalCard;
