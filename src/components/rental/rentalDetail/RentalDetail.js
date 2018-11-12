121320e775850d29d57e3950888eb04e4e8aa0ce
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-12 16:48:05 +0530

    Rental Detail More Components Separation

--- src/components/rental/rentalDetail/RentalDetail.js
+++ src/components/rental/rentalDetail/RentalDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import { connect } from 'react-redux';
+import { RentalDetailInfo } from './RentalDetailParts/RentalDetailInfo';
 
 import * as actions from '../../../actions';
 
@@ -31,35 +32,7 @@ class RentalDetail extends Component {
                     <div className='details-section'>
                         <div className='row'>
                             <div className='col-md-8'>
-                                <div className='rental'>
-                                    <h2 className='rental-type'>{rental.shared} {rental.category}</h2>
-                                    <h1 className='rental-title'>{rental.title}</h1>
-                                    <h2 className='rental-city'>{rental.city}</h2>
-                                    <div className='rental-room-info'>
-                                        <span><i className='fa fa-building'></i>{rental.bedrooms} bedrooms</span>
-                                        <span><i className='fa fa-user'></i>{rental.bedrooms + 4} guests</span>
-                                        <span><i className='fa fa-bed'></i>{rental.bedrooms + 2} beds</span>
-                                    </div>
-                                    <p className='rental-description'>
-                                        {rental.description}
-                                    </p>
-                                    <hr></hr>
-                                    <div className='rental-assets'>
-                                        <h3 className='title'>Assets</h3>
-                                        <div className='row'>
-                                            <div className='col-md-6'>
-                                                <span><i className='fa fa-asterisk'></i> Cooling</span>
-                                                <span><i className='fa fa-thermometer'></i> Heating</span>
-                                                <span><i className='fa fa-location-arrow'></i> Iron</span>
-                                            </div>
-                                            <div className='col-md-6'>
-                                                <span><i className='fa fa-desktop'></i> Working area</span>
-                                                <span><i className='fa fa-cube'></i> Washing machine</span>
-                                                <span><i className='fa fa-cube'></i> Dishwasher</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <RentalDetailInfo rental={rental}/>
                             </div>
                             <div className='col-md-4'>BOOKING</div>
                         </div>
