fd74d0dfee1a9545284a683883bf49f2a23d716b
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-12 14:25:29 +0530

    Filled our Database with fake Data

---
+++ server/fake-db.js
@@ -0,0 +1,50 @@
+const Rental = require('./models/rental');
+
+class FakeDb {
+    constructor() {
+        this.rentals = [
+            {
+                title: "Nice view on ocean",
+                city: "San Francisco",
+                street: "Main street",
+                category: "condo",
+                image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+                bedrooms: 4,
+                description: "Very nice apartment in center of the city.",
+                dailyRate: 43
+            },
+            {
+                title: "Modern apartment in center",
+                city: "New York",
+                street: "Time Square",
+                category: "apartment",
+                image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+                bedrooms: 1,
+                description: "Very nice apartment in center of the city.",
+                dailyRate: 43
+            },
+            {
+                title: "Old house in nature",
+                city: "Spisska Nova Ves",
+                street: "Banicka 1",
+                category: "house",
+                image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+                bedrooms: 5,
+                description: "Very nice apartment in center of the city.",
+                dailyRate: 23
+            }
+        ]
+    }
+
+    pushRentalsToDb() {
+        this.rentals.forEach((rental) => {
+            const newRental = new Rental(rental);
+            newRental.save();
+        });
+    }
+    seedDb() {
+        this.pushRentalsToDb();
+    }
+}
+
+module.exports = FakeDb; 
--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/dev');
 const Rental = require('./models/rental');
+const FakeDb = require('./fake-db');
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true });
+
+mongoose.connect(config.DB_URI, { useNewUrlParser: true })
+    .then((sucess) => {
+        const fakeDb = new FakeDb();
+        fakeDb.seedDb(); 
+    });
 
 const app = express();
 
