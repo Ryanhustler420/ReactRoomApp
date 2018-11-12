de0da1a9cd8e58785c58a2cb6facd31ca928a010
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-12 14:20:05 +0530

    Mongoose Rental Model Created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/dev');
+const Rental = require('./models/rental');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true });
 
