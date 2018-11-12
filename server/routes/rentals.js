2bf7a1249c9c05cb90add24b81a8456ed4191343
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-12 14:42:59 +0530

    Handle Error if rental id not Found!

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -11,6 +11,9 @@ router.get('',(req,res) => {
 router.get('/:id', (req,res) => {
     const rentalId = req.params.id;
     Rental.findById(rentalId, (err,foundRental) => {
+        if(err){
+            return res.status(422).send({error:[{title:'Rental Error!', detail: 'Could not find Rental!'}]});
+        }
         res.json(foundRental);
     });
 });
