ed346e91d8f61ac740d3adf7eb5fbf3353647171
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-12 12:14:40 +0530

    Redux Chrome Extension

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,19 @@
-import * as redux from 'redux';
 import { rentalReducer, selectedRentalReducer } from './rental.reducer';
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
 export const init = () => {
-    const reducer = redux.combineReducers({
+    const reducer = combineReducers({
         rentals:rentalReducer,
         rental:selectedRentalReducer
     });
 
-    const store = redux.createStore(reducer, redux.applyMiddleware(thunk));
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+    const store = createStore(
+        reducer,
+        composeEnhancers(applyMiddleware(thunk)),
+    );
 
     return store;
 }
