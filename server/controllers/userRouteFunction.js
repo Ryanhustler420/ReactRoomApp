fc1918b1a4c224de52eabd51bfa669d2dc0db463
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-14 22:59:22 +0530

    Starting Register

--- server/controllers/userRouteFunction.js
+++ server/controllers/userRouteFunction.js
@@ -1,8 +1,50 @@
+const User = require('../models/user');
+
 exports.auth = function(req, res) {
     res.json({'auth':'route working'});
 }
 
 exports.register = function(req, res) {
     const {username, email, password, passwordConfirm} = req.body;
-    res.json({username, email, password, passwordConfirm});
+
+    // if email or password is empty than throw this error
+    if(!password || !email){
+        return res.status(422).send(createErrorObject('Data Missing','Provide email and password!'));
+    }
+
+    // if both password not match than throw this error 
+    if(password !== passwordConfirm){
+        return res.status(422).send(createErrorObject('Invalid password','Password is not as same as confirmation!'));
+    }
+
+    // searching into the database with givin email id
+    User.findOne({email}, async (error, user) => {
+
+        // if something went wrong with database while searching user than we'll throw this error
+        if(error)
+            return res.status(422).send(createErrorObject('Invalid password','Password is not as same as confirmation!'));
+
+        // if user already exists in the data base
+        if(user)
+            return res.status(422).send(createErrorObject('Invalid Email',`User with this email ${email} already exist!`));
+
+        //creating new user after checking into database if user not exist with the given email id
+        const newUser = new User({ username, email, password });
+
+        // saving to the database async
+        await newUser.save();
+
+        // sending response back if registration successfull
+        res.json({'status':true});
+    });
+}
+
+const createErrorObject = (title,detail) => {
+    return {
+        errors: [
+            {
+                title,detail
+            }
+        ]
+    }
 }
