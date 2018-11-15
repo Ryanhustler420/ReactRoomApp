fb184592d79cc28165ac9793204aa3cdcaa8ccfc
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-16 01:21:28 +0530

    Update UI depending on auth state

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = (props) => {
-    return(
-        <nav className='navbar navbar-dark navbar-expand-lg'>
-            <div className='container'>
-                <Link className='navbar-brand' to='/rentals'>React Rooms</Link>
-                <form className='form-inline my-2 my-lg-0'>
-                    <input className='form-control mr-sm-2 bwm-search' type='search' placeholder="Try 'New York'" aria-label='Search'></input>
-                    <button className='btn btn-outline-success my-2 my-sm-0 btn-bwm-search' type='submit'>Search</button>
-                </form>
-                <button className='navbar-toggler' type='button' data-toggle='collapse' data-targer='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup'
-                        aria-expanded='false' aria-label='Toggle navigation'>
-                    <span className='navbar-toggler-icon'></span>
-                </button>
-                <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-                    <div className='navbar-nav ml-auto'>
-                        <Link className='nav-item nav-link active' to='/login'>Login <span className='sr-only'>(current)</span></Link>
-                        <Link className='nav-item nav-link' to='/register'>Register</Link>
-                        <p className="nav-item nav-link" onClick={props.logout}>Logout</p>
+class Header extends Component {
+
+    renderAuthBtn() {
+        const { isAuth } = this.props.auth;
+
+        if(isAuth){
+            return <a className="nav-item nav-link clickable" onClick={this.props.logout}>Logout</a>
+        }
+
+        return(
+            <React.Fragment>
+                <Link className='nav-item nav-link active' to='/login'>Login <span className='sr-only'>(current)</span></Link>
+                <Link className='nav-item nav-link' to='/register'>Register</Link>
+            </React.Fragment> 
+        )
+    }
+
+    render() {
+        return(
+            <nav className='navbar navbar-dark navbar-expand-lg'>
+                <div className='container'>
+                    <Link className='navbar-brand' to='/rentals'>React Rooms</Link>
+                    <form className='form-inline my-2 my-lg-0'>
+                        <input className='form-control mr-sm-2 bwm-search' type='search' placeholder="Try 'New York'" aria-label='Search'></input>
+                        <button className='btn btn-outline-success my-2 my-sm-0 btn-bwm-search' type='submit'>Search</button>
+                    </form>
+                    <button className='navbar-toggler' type='button' data-toggle='collapse' data-targer='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup'
+                            aria-expanded='false' aria-label='Toggle navigation'>
+                        <span className='navbar-toggler-icon'></span>
+                    </button>
+                    <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+                        <div className='navbar-nav ml-auto'>
+                            {this.renderAuthBtn()}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>   
-    )
+            </nav>   
+        )
+    }
 }
 
-export default Header;
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(Header);
