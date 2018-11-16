b5c332c3da6d16427e7dd6fa940a66b2e897ee4d
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-16 22:12:01 +0530

    update rental route functionallity

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -4,18 +4,23 @@ const Rental = require('../models/rental');
 const UserCtrl = require('../controllers/userRouteFunction');
 
 router.get('',(req,res) => {
-    Rental.find({}, (err,rentals) => {
-        res.json(rentals);
+    Rental.find({})
+    .select('-bookings')
+    .exec(function(err,foundRental){
+        res.json(foundRental);
     });
 });
 
 router.get('/:id', (req,res) => {
     const rentalId = req.params.id;
 
-    Rental.findById(rentalId, (err, foundRental) => {
-        if(err){
-            return res.status(422).send({error:[{title:'Rental Error!', detail: 'Could not find Rental!'}]});
-        }
+    Rental.findById( rentalId )
+        .populate('user', 'username -_id')
+        .populate('bookings', 'startAt endAt -_id')
+            .exec(function(err,foundRental){
+                if(err){
+                    return res.status(422).send({error:[{title:'Rental Error!', detail: 'Could not find Rental!'}]});
+                }
         return res.json(foundRental);
     });
 });
