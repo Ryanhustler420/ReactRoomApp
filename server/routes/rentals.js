767e784c4ee9216d2d079e1dff4aad7b014e6909
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-20 19:37:10 +0530

    Search city endpoint start

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -2,13 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
 const UserCtrl = require('../controllers/userRouteFunction');
+const { normalizeErrors } = require('../helper/errorParser');
 
 router.get('',(req,res) => {
-    Rental.find({})
-    .select('-bookings')
-    .exec(function(err,foundRental){
-        res.json(foundRental);
-    });
+    const city = req.query.city;
+
+    if(city){
+        Rental.find({city: city.toLowerCase()})
+            .select('-bookings') // this means we dont want bookings array here
+            .exec(function(error, filterRentals){
+                if(error)
+                    return res.status(422).send({error:normalizeErrors(error.errors)});
+
+                // if lenght of foundRental is 0 thats means we dont got any rental by search query
+                if(filterRentals.length == 0)
+                    return res.status(422).send({error:createErrorObject('No Rental Found!',`There are no rentals for city ${city}`)});
+
+                // if we got this line that means we found that renatal and we we'll send that response
+                return res.json(filterRentals);
+            })
+    }else{
+        Rental.find({})
+        .select('-bookings')
+        .exec(function(err,foundRental){
+            res.json(foundRental);
+        });
+    }
 });
 
 router.get('/:id', (req,res) => {
@@ -24,5 +43,16 @@ router.get('/:id', (req,res) => {
         return res.json(foundRental);
     });
 });
+
+// create error message object as a helper method
+const createErrorObject = (title,detail) => {
+    return {
+        errors: [
+            {
+                title,detail
+            }
+        ]
+    }
+}
 
 module.exports = router; 
