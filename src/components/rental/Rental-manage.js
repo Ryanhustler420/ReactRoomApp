8b89435aca5ef95a20ca1068a7ec1d1df59e8414
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-27 18:59:24 +0530

    Manage Rentals Layout

--- src/components/rental/Rental-manage.js
+++ src/components/rental/Rental-manage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import * as actions from '../../actions';
+import { Link } from 'react-router-dom';
+import RentalManageCard from './RentalManageCard';
 
 export default class RentalManage extends Component {
 
@@ -8,32 +10,45 @@ export default class RentalManage extends Component {
 
         this.state = {
             userRental: [],
-            errors: []
+            errors: [],
+            isFetching: false
         }
     }
 
     componentWillMount() {
+        this.setState({isFetching:true});
         actions.getUserRentals().then(
-            result => this.setState({userRental: result}),
-            errors => this.setState({errors})
+            result => this.setState({userRental: result, isFetching: false}),
+            errors => this.setState({errors, isFetching: false})
         )
     }
 
-    renderData() {
-        const { userRental, errors } = this.state;
+    renderData(userRental) {
+        return userRental.map((rental, index) => <RentalManageCard key={index} rental={rental} />)
+    }
 
-        if(userRental){
-            return userRental.map((rental, index) => <p key={index} >{rental.title}</p>)
-        }else if(!userRental || errors){
-            return <h1>Sorry, But there are no rental!</h1>
-        }
+    renderError(isFetching, userRental) {
+        console.log('trigger');
+        return (
+            !isFetching && userRental.length === 0 &&
+            <div className='alert alert-warning'>
+                You dont have any rentals currently created. If you want advertis your property
+                Please follow this link.
+                <Link className='btn btn-bwm' style={{'marginLeft': '10px'}} to={'/rentals/new'}>Register Rental</Link>
+            </div>
+        )
     }
 
     render() {
+        const {userRental, isFetching} = this.state;
         return (
-            <div>
-                {this.renderData()}
-            </div>
+            <section id='userRentals'>
+                <h1 className='page-title'>My Rentals</h1>
+                <div className='row'>
+                    {this.renderData(userRental)}
+                </div>
+                    {this.renderError(isFetching, userRental)}
+            </section>
         )
     }
 }
---
+++ src/components/rental/RentalManageCard.js
@@ -0,0 +1,24 @@
+import  React  from 'react';
+import { Link } from 'react-router-dom';
+
+const RentalManageCard = (props) => {
+
+    const { rental } = props;
+
+    return (
+        <div className='col-md-4'>
+            <div className='card text-center'>
+                <div className='card-block'>
+                    <h4 className='card-title'>{rental.title} - {rental.city}</h4>
+                    <Link className='btn btn-bwm' to={`/rentals/${rental._id}`}>Go to Rental</Link>
+                    <button className='btn btn-bwm'> Bookings </button>
+                </div>
+                <div className='card-footer text-muted'>
+                    Created at {rental.createdAt}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RentalManageCard;
