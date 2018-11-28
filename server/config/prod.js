8aa25f24bcb49c0ef6a1237e77b43e00c6fcba53
Gaurav Gupta <gouravgupta840@gmail.com>
2018-11-28 21:49:34 +0530

    Package.json and Config variables

---
+++ server/config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_URI:'',
+    SECRET:''
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
+const config = require('./config');
 const Rental = require('./models/rental');
 const FakeDb = require('./fake-db');
 
