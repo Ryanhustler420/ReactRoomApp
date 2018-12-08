13197de07ad9a073ee77aa4d614b8af24e341965
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-08 17:10:37 +0530

    Editable Input Handle Active State

--- src/components/shared/editable/EditableInput.js
+++ src/components/shared/editable/EditableInput.js
@@ -22,13 +22,47 @@ export class EditableInput extends React.Component {
         });
     }
 
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    disableEdit() {
+        this.setState({isActive: false})
+    }
+
+    enableEdit() {
+        this.setState({isActive: true});
+    }
+
+    renderComponentView() {
+        const { value, isActive } = this.state;
+
+        if(isActive) {
+            return (
+                <React.Fragment>
+                    <input onChange={(event) => this.handleChange(event)} value={value}/>
+                    <button type="button" onClick={() => this.disableEdit()} className='btn btn-warning'> Close </button>
+                </React.Fragment>
+            )
+        }
+
+        return (
+            <React.Fragment>
+                <p> {value} </p>
+                <button type="button" onClick={() => this.enableEdit()} className='btn btn-warning'> Edit </button>
+            </React.Fragment>
+        )
+    }
+
     render() {
 
         const { value } = this.state;
 
         return (
             <div>
-                <input value={value}/>
+            {this.renderComponentView()}
             </div>
         )
     }
