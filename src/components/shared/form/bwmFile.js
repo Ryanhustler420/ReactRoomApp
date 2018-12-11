5da432e1c5784f4ec229d186cff1872a7b6c4963
Gaurav Gupta <gouravgupta840@gmail.com>
2018-12-11 15:08:17 +0530

    Style input field

--- src/components/shared/form/bwmFile.js
+++ src/components/shared/form/bwmFile.js
@@ -8,7 +8,7 @@ export default class BwmFile extends Component {
         this.setupReader();
 
         this.state = {
-            selectedFile : {},
+            selectedFile : undefined,
             imageBase64: ''
         }
 
@@ -24,7 +24,7 @@ export default class BwmFile extends Component {
     }
 
     onChange(event){
-        const { input: {onChange} } = this.props;
+        // const { input: {onChange} } = this.props;
 
         // onChange('https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg');
 
@@ -36,23 +36,55 @@ export default class BwmFile extends Component {
         }
     }
 
-    render() {
+    uploadImage() {
 
-        const {label, meta: {touched, error} } = this.props;
+    }
+
+    render() {
+        const { label, meta: {touched, error} } = this.props;
+        const { selectedFile, imageBase64 } = this.state;
 
         return (
-            <div className="form-group">
-                <label>{label}</label>
-                <div className="input-group">
+            <div className="img-upload-container">
+                <label className='img-upload btn btn-bwm'>
+                    <span className='upload-text'>Select Image</span>
                     <input 
                         type='file' 
                         accept='.jpg, .png, .jpeg' 
                         onChange={this.onChange} 
                     />
-                </div>
-                {touched &&
-                    ((error && <div className="alert alert-danger">{error}</div>))}
+                </label>
+
+                { selectedFile && 
+                    <button className='btn btn-success img-upload' 
+                            type='button' 
+                            disabled={!selectedFile}
+                            onClick={() => this.uploadImage()}
+                            >Upload</button>
+                }
             </div>
         )
     }
+
+
+
+    // render() {
+
+    //     const {label, meta: {touched, error} } = this.props;
+
+    //     return (
+    //         <div className="form-group">
+    //             <label>{label}</label>
+    //             <div className="input-group">
+    //                 <input 
+    //                     type='file' 
+    //                     accept='.jpg, .png, .jpeg' 
+    //                     onChange={this.onChange} 
+    //                 />
+    //             </div>
+    //             {touched &&
+    //                 ((error && <div className="alert alert-danger">{error}</div>))}
+    //         </div>
+    //     )
+    // }
 }
